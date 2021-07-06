905a023d069070d7123480131b0290a04184f405
Hannah Greven <--global>
2021-07-06 11:42:28 +0200

    Switched to unsafe_load knowing about the security issues to not show warning each time

--- parse_config.py
+++ parse_config.py
@@ -71,7 +71,7 @@ class ConfigParser:
 
         #config = read_json(cfg_fname)
         fp_yaml =  open('./config.yaml')
-        config = yaml.load(fp_yaml)
+        config = yaml.unsafe_load(fp_yaml) # changed from load (deprecated due to security issues) to unsafe_load (does not trigger warning, security issue accepted)
         if args.config and resume:
             # update new config for fine-tuning
             config.update(read_json(args.config))
